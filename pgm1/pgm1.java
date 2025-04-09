7ddb1cce853678aa932227771083a9ec19628c66
01fe23bca056 <01fe23bca056@kletech.ac.in>
2025-04-09 10:16:42 +0530

    Created a simple calculator

--- pgm1/pgm1.java
+++ pgm1/pgm1.java
@@ -1,7 +1,44 @@
 package pgm1;
 
+import java.util.Scanner;
+
 public class pgm1 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!\n");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Simple Calculator");
+        System.out.println("Choose an operation: +, -, *, /");
+        char operation = scanner.next().charAt(0);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result = 0;
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operation.");
+        }
+
+        System.out.println("The result is: " + result);
+        scanner.close();
     }
 }
