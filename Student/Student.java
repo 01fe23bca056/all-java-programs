ae887275d4a1a90d19134c43b40d82290e423e1d
01fe23bca056 <01fe23bca056@kletech.ac.in>
2025-04-16 12:23:27 +0530

    Added Student basic details

---
+++ Student/Student.java
@@ -0,0 +1,9 @@
+package Student;
+
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("Student Details:");
+        System.out.println("Name: Vijay");
+        System.out.println("USN: 01fe23bca056");
+    }
+}
